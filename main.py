7c08d85bb32eaa70c44e77182076f46e94877f7a
xXbest.sw.dev59Xx <bestswdev@yahoo.it>
2021-03-21 00:55:47 +0100

    now its nicer :)

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-s = input()
-
-print(s.upper())
+if __name__ == "__main__":
+    print("Please give me a string!!!!")
+    input_string = input()
+    capitalized_string = input_string.upper()
+    print("Here is your CAPITALIZED string!!! Enjoy ur day!! :D")
+    print(capitalized_string)
