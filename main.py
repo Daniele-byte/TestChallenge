24b6caf73cce1503a9b67e7639821d972c597a1f
xXbest.sw.dev59Xx <bestswdev@yahoo.it>
2021-03-21 16:44:37 +0100

    typo

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import argparse
 
 def parse_arguments():
-    parser = argparse.ArgumentParser(description="Do you like CAPITALIZED strings??? Here's a tool to capitalize them! jst put  a string and it will be capitlized")
+    parser = argparse.ArgumentParser(description="Do you like CAPITALIZED strings??? Here's a tool to capitalize them! just put  a string and it will be capitalized")
     parser.add_argument('N', type=int, help='how many strings??')
     return parser.parse_args()
 
