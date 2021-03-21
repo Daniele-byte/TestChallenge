0dbea745f0a016be26fdf3d6d2c6066f9a901250
xXbest.sw.dev59Xx <bestswdev@yahoo.it>
2021-03-21 16:43:49 +0100

    get how many strings from terminal

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
+import argparse
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Do you like CAPITALIZED strings??? Heres a tool to capitalize them! jst put  a string and it will be capitlized')
+    parser.add_argument('N', type=int, help='how many strings??')
+    return parser.parse_args()
+
+
 def capitalize(string: str):
     return input_string.upper()
 
 
 if __name__ == "__main__":
-    print("Please give me a string!!!!")
-    input_string = input()
-    capitalized_string = capitalize(input_string)
-    print("Here is your CAPITALIZED string!!! Enjoy ur day!! :D")
-    print(capitalized_string)
+    args = parse_arguments()
+    for _ in range(args.N):
+        print("Please give me a string!!!!")
+        input_string = input()
+        capitalized_string = capitalize(input_string)
+        print("Here is your CAPITALIZED string!!! Enjoy ur day!! :D")
+        print(capitalized_string)
