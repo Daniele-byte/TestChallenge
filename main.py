838a699525114e17a718abd1e285aa15a2cd0afa
xXbest.sw.dev59Xx <bestswdev@yahoo.it>
2021-03-21 15:46:01 +0100

    stuff

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+
+def capitalize_string(string: str):
+    return input_string.lower()
+
+
 if __name__ == "__main__":
     print("Please give me a string!!!!")
     input_string = input()
-    capitalized_string = input_string.upper()
+    capitalized_string = capitalize_string(input_string)
     print("Here is your CAPITALIZED string!!! Enjoy ur day!! :D")
     print(capitalized_string)
